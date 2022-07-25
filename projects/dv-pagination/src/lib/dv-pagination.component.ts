4cb0c6fd0ca924f938b8afa8957e2d7c9917c8ec
Dharmendra <pateldharmendra123@gmail.com>
2022-07-25 10:50:08 +0530

    version changes

--- projects/dv-pagination/src/lib/dv-pagination.component.ts
+++ projects/dv-pagination/src/lib/dv-pagination.component.ts
@@ -6,21 +6,23 @@ import dvpaginate from './dv-paginate';
   selector: 'dv-pagination',
   template: `<ul *ngIf="pager.pages && pager.pages.length" class="pagination">
   <li [ngClass]="{disabled:pager.currentPage === 1}" class="page-item first-item">
-      <a (click)="setPage(1)" class="page-link">First</a>
+  <a (click)="setPage(1)" class="page-link">First</a>
   </li>
   <li [ngClass]="{disabled:pager.currentPage === 1}" class="page-item previous-item">
-      <a (click)="setPage(pager.currentPage - 1)" class="page-link">Previous</a>
+  <a (click)="setPage(pager.currentPage - 1)" class="page-link">Previous</a>
   </li>
   <li *ngFor="let page of pager.pages" [ngClass]="{active:pager.currentPage === page}" class="page-item number-item">
-      <a (click)="setPage(page)" class="page-link">{{page}}</a>
+  <a (click)="setPage(page)" class="page-link">{{page}}</a>
   </li>
   <li [ngClass]="{disabled:pager.currentPage === pager.totalPages}" class="page-item next-item">
-      <a (click)="setPage(pager.currentPage + 1)" class="page-link">Next</a>
+  <a (click)="setPage(pager.currentPage + 1)" class="page-link">Next</a>
   </li>
   <li [ngClass]="{disabled:pager.currentPage === pager.totalPages}" class="page-item last-item">
-      <a (click)="setPage(pager.totalPages)" class="page-link">Last</a>
+  <a (click)="setPage(pager.totalPages)" class="page-link">Last</a>
   </li>
-</ul>`
+  </ul>`,
+  styles: [
+  ]
 })
 
 export class DvPaginationComponent implements OnInit,OnChanges {
--- projects/dv-pagination/src/lib/dv-pagination.module.ts
+++ projects/dv-pagination/src/lib/dv-pagination.module.ts
@@ -1,16 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DvPaginationComponent } from './dv-pagination.component';
 
  
 
 @NgModule({
-  declarations: [
-    DvPaginationComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    DvPaginationComponent
-  ]
+  declarations: [DvPaginationComponent],
+  imports: [CommonModule],
+  exports: [DvPaginationComponent]
 })
 export class DvPaginationModule { }
